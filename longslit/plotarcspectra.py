ab3c8ad42d0585f4061c9a1154f887d29cabccf3
crawfordsm <crawfordsm@gmail.com>
2012-06-22 19:36:06 +0200

    Added a bit more comments to the script

--- longslit/plotarcspectra.py
+++ longslit/plotarcspectra.py
@@ -8,7 +8,16 @@ from PySpectrograph.Models import RSSModel
 from PySpectrograph.Spectra import Spectrum
 
 def plotarcspectra(arclist='Ne.txt', grating='PG0900', gratang=15.0, camang=30.0, slit=1.5, xbin=2, ybin=2):
-   """Plot an Arc Line list for a given RSS set up"""
+   """Plot an Arc Line list for a given RSS set up 
+      arclist--an arc line list in the format of 'wavelength flux' for arc lines
+      grating--name of the grating
+      gratang--angle of the grating
+      camang--angle of the camera (articulation angle)
+      slit--slit width in arcseconds
+      xbin--xbinning
+      ybin--ybinning
+
+   """
    rss=RSSModel.RSSModel(grating_name=grating, gratang=gratang, camang=camang, 
                       slit=slit, xbin=xbin, ybin=ybin)
 
@@ -51,6 +60,13 @@ def plotarcspectra(arclist='Ne.txt', grating='PG0900', gratang=15.0, camang=30.0
 
 
 if __name__=='__main__':
+   """Example of running the task
+   python plotarcspectra.py [line list] [grating name] [grating angle] [camera angle] [slit] [xbin] [ybin]
+
+   Here is an example of running it:
+   python plotarcspectra.py Ne.txt PG0900  15.0 30.0 1.5 2 2
+
+   """
    #create the spectrograph model
    arclist=sys.argv[1]
    grating=sys.argv[2]
